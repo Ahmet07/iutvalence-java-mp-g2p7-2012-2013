1d7f83c436fdad574865e745c23fd619a9ccc993
ozdemira <ozdemira@pc-123-01.iut-valence.fr>
2012-09-25 15:30:04 +0200

    Mise à jour (1) -Classe carte

--- src/fr/iutvalence/java/projets/blackjack/Carte.java
+++ src/fr/iutvalence/java/projets/blackjack/Carte.java
@@ -1,43 +1,154 @@
 package fr.iutvalence.java.projets.blackjack;
 
+/**
+ * @author Ozdemir Ahmet & Mezgueldi Samih
+ * 
+ *La classe Carte permet de définir toutes les caractéristiques d'une carte à jouer
+ *
+ */
 public class Carte
+
 {
+	
+	
+	/**
+	 * Constante représentant la couleur "Carreau"
+	 */	
 	public final static int CARREAU = 1;
+	
+	/**
+	 * Constante représentant la couleur "Coeur"
+	 */	
 	public final static int COEUR = 2;
+	
+	/**
+	 * Constante représentant la couleur "Pique"
+	 */	
 	public final static int PIQUE = 3;
+	
+	/**
+	 * Constante représentant la couleur "Trèfle"
+	 */	
 	public final static int TREFLE = 4;
 	
+	
+	
+
+	/**
+	 * Constante représentant la valeur "AS"
+	 */	
 	public final static int AS = 1;
+	
+	/**
+	 * Constante représentant la valeur "DEUX"
+	 */	
 	public final static int DEUX = 2;
+	
+	/**
+	 * Constante représentant la valeur "TROIS"
+	 */	
 	public final static int TROIS = 3;
+	
+	/**
+	 * Constante représentant la valeur "QUATRE"
+	 */	
 	public final static int QUATRE = 4;
+	
+	/**
+	 * Constante représentant la valeur "CINQ"
+	 */	
 	public final static int CINQ = 5;
+	
+	/**
+	 * Constante représentant la valeur "SIX"
+	 */	
 	public final static int SIX = 6;
+	
+	/**
+	 * Constante représentant la valeur "SEPT"
+	 */	
 	public final static int SEPT = 7;
+	
+	/**
+	 * Constante représentant la valeur "HUIT"
+	 */	
 	public final static int HUIT = 8;
+	
+	/**
+	 * Constante représentant la valeur  "NEUF"
+	 */	
 	public final static int NEUF = 9;
+	
+	/**
+	 * Constante représentant la valeur  "DIX"
+	 */	
 	public final static int DIX = 10;
+	
+	/**
+	 * Constante représentant la valeur  "VALLET"
+	 */	
 	public final static int VALET = 11;
+	
+	/**
+	 * Constante représentant la valeur  "DAME"
+	 */	
 	public final static int DAME = 12;
+	
+	/**
+	 * Constante représentant la valeur  "ROI"
+	 */	
 	public final static int ROI = 13;
 	
 	
+	
+	
+	
+	/**
+	 * Declaration de l'entier couleur
+	 */
 	private int couleur;
+	
+	/**
+	 * Declaration de l'entier valeur
+	 */
 	private int valeur;
+	
+	/**
+	 * Declaration de l'entier symbole
+	 */
 	private int symbole;
 	
-	public Carte(int c, int s, int v){
-
+	
+	/**
+	 * Composition d'une carte
+	 */
+	public Carte(int c, int s, int v)
+	{
 		this.couleur = c;
 		this.symbole = s;				
 		this.valeur  = v;
 	}
 	
-	public String toString()
+	
+	/**
+	 * @param v
+	 * @return La couleur de la carte par rapport à sa valeur
+	 */
+	private String couleurToString(int v)
 	{
-		String s = "Carte de couleur "+ this.couleur;
-		s += "et de valeur " + this.symbole;
-		return s;
+	if (v == 1) return "Carreau";
+	if (v == 2) return "Coeur";
+	if (v == 3) return "Pique";
+	if (v == 4) return "Trèfle";
+	else return "Erreur";
 	}
 	
+	
+	public String toString()
+	{
+		String s = "Carte de couleur "+ couleurToString(this.couleur);
+		s += " et de valeur " + this.symbole;
+		return s;
+	}
+
 }
--- src/fr/iutvalence/java/projets/blackjack/Creer_Carte.java
+++ src/fr/iutvalence/java/projets/blackjack/Creer_Carte.java
@@ -1,13 +1,17 @@
 package fr.iutvalence.java.projets.blackjack;
 
+/**
+ * @author Ozdemir Ahmet & Mezgueldi Samih
+ *
+ */
 public class Creer_Carte
 {
 
 
 		public static void main (String[] args){
 			
-			Carte c = new Carte(Carte.CARREAU, Carte.CINQ, 5);
-			
+			Carte c = new Carte(Carte.TREFLE, Carte.CINQ, 5);
+			System.out.println(c.toString());
 		} 				
 
 
